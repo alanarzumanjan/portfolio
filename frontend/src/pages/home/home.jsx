1e9b0345f1637df98a5ee133591aa99f5cef44c4
Alan Arzumanjan <alanarzumanjan@gmail.com>
2025-06-19 22:05:55 +0300

    first code

---
+++ frontend/src/pages/home/home.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Home() {
+
+    return (
+        <div className="home">
+            <h1>Hello, I'am Alan Arzumanjan and this is my portfolio website.</h1>
+            <p>
+                Feel free to explore my projects and get to know more about me, myskills and experience.
+            </p>
+        </div>
+    );
+
+}
