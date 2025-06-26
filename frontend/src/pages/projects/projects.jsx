454c625dc3e416efa88104ef8f223db2d6a7890c
Alan Arzumanjan <alanarzumanjan@gmail.com>
2025-06-26 04:31:42 +0300

    Projects maket

--- frontend/src/pages/projects/projects.jsx
+++ frontend/src/pages/projects/projects.jsx
@@ -1,5 +1,5 @@
-import React from "react";
 import "./projects.css";
+import meltwaterImage from "../../assets/projects/meltwater.png";
 
 function Projects() {
 
@@ -7,11 +7,84 @@ function Projects() {
         <div className="projects">
             <h1>Projects</h1>
             <p> I have a few projects that I have worked on in the past.</p>
+            <div className="projects-container">
+                <div className="project">
+                    <h2>Meltwater Website</h2>
+                    <img src={meltwaterImage} alt="meltwater website" />
+                    <p>
+                        Frontend test version for the Meltwater project.
+                    </p>
+                    <a href="https://github.com/alanarzumanjan/meltwater_website"
+                        target="_blank" rel="noopener noreferrer"
+                        className="project-button">
+                        Repository
+                    </a>
+                </div>
 
-            <div className="project">
-                <h2>Project 1</h2>
-                <img src="{projectImage}" alt="Terminal project" />
-                <p> project_description </p>
+                <div className="project">
+                    <h2>Meltwater Website</h2>
+                    <img src={meltwaterImage} alt="meltwater website" />
+                    <p>
+                        Frontend test version for the Meltwater project.
+                    </p>
+                    <a href="https://github.com/alanarzumanjan/meltwater_website"
+                        target="_blank" rel="noopener noreferrer"
+                        className="project-button">
+                        Repository
+                    </a>
+                </div>
+
+                <div className="project">
+                    <h2>Meltwater Website</h2>
+                    <img src={meltwaterImage} alt="meltwater website" />
+                    <p>
+                        Frontend test version for the Meltwater project.
+                    </p>
+                    <a href="https://github.com/alanarzumanjan/meltwater_website"
+                        target="_blank" rel="noopener noreferrer"
+                        className="project-button">
+                        Repository
+                    </a>
+                </div>
+
+                <div className="project">
+                    <h2>Meltwater Website</h2>
+                    <img src={meltwaterImage} alt="meltwater website" />
+                    <p>
+                        Frontend test version for the Meltwater project.
+                    </p>
+                    <a href="https://github.com/alanarzumanjan/meltwater_website"
+                        target="_blank" rel="noopener noreferrer"
+                        className="project-button">
+                        Repository
+                    </a>
+                </div>
+
+                <div className="project">
+                    <h2>Meltwater Website</h2>
+                    <img src={meltwaterImage} alt="meltwater website" />
+                    <p>
+                        Frontend test version for the Meltwater project.
+                    </p>
+                    <a href="https://github.com/alanarzumanjan/meltwater_website"
+                        target="_blank" rel="noopener noreferrer"
+                        className="project-button">
+                        Repository
+                    </a>
+                </div>
+
+                <div className="project">
+                    <h2>Meltwater Website</h2>
+                    <img src={meltwaterImage} alt="meltwater website" />
+                    <p>
+                        Frontend test version for the Meltwater project.
+                    </p>
+                    <a href="https://github.com/alanarzumanjan/meltwater_website"
+                        target="_blank" rel="noopener noreferrer"
+                        className="project-button">
+                        Repository
+                    </a>
+                </div>
             </div>
         </div>
     );
